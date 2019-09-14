38ca157ca8b53ebf5f2d416522a25401451184d1
fiftie <kumavichi.t@gmail.com>
2019-09-14 21:17:46 +0900

    Initial Commit

---
+++ bustool/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bustool
+//
+//  Created by Tatsuyoshi Igarashi on 2019/09/14.
+//  Copyright © 2019 Tatsuyoshi Igarashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
